deb294b8972a809cc08b0705486b94e2f33be91d
doy-lee <doylet@protonmail.com>
2019-01-07 11:07:23 +1100

    Comma seperate the circulating supply (regression)

--- src/page.h
+++ src/page.h
@@ -1265,7 +1265,7 @@ index2(uint64_t page_no = 0, bool refresh_page = false)
                 {"blk_no"    , emission_blk_no},
                 {"amount"    , emission_coinbase},
                 {"fee_amount", emission_fee},
-                {"circulating_supply", CurrentBlockchainStatus::circulating_supply}
+                {"circulating_supply", lokeg::make_comma_sep_number(CurrentBlockchainStatus::circulating_supply)}
         };
 
         if (CurrentBlockchainStatus::circulating_supply_calc_from_height + 10 < core_storage->get_current_blockchain_height())
